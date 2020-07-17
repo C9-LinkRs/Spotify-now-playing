00230e8a652f13d8e2fdf0582a0f3cb840af3f2a
C9-LinkRs <linkrsfan@gmail.com>
2020-07-17 09:02:13 +0200

    Background image in card

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,13 +1,12 @@
 <template>
   <vs-row vs-justify="center">
     <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="2.1">
-      <vs-card actionable class="cardx">
-        <div slot="media">
-          <img :src="image">
-        </div>
-        <div>
-          <h2>{{ title }}</h2>
-          <h3>{{ artist }}</h3>
+      <vs-card actionable class="cardx" id="border" :style="{ 'background-image': 'url(' + image + ')' }">
+        <div id="image_block">
+          <div>
+            <h2>{{ title }}</h2>
+            <h3>{{ artist }}</h3>
+          </div>
         </div>
       </vs-card>
     </vs-col>
@@ -48,4 +47,15 @@
 </script>
 
 <style scoped>
+  #border {
+    border-radius: 10px;
+    background-size: 300px;
+    background-repeat: no-repeat;
+    width: 300px;
+  }
+
+  #image_block {
+    height: 280px; 
+  }
+  
 </style>
