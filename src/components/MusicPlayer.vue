2cd73d61f85d43d82695bdc7198b5fa1abc9873b
C9-LinkRs <linkrsfan@gmail.com>
2020-07-17 07:52:21 +0200

    essencial data plotted in front end

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,17 +1,17 @@
 <template>
-  <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-    <vs-card actionable class="cardx">
-      <div slot="header">
-        <h3>{{ title }}</h3>
-      </div>
-      <div slot="media">
-        <img :src="require('@/assets/no_song.png')">
-      </div>
-      <div>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-      </div>
-    </vs-card>
-  </vs-col>
+  <vs-row vs-justify="center">
+    <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="2.1">
+      <vs-card actionable class="cardx">
+        <div slot="media">
+          <img :src="image">
+        </div>
+        <div>
+          <h2>{{ title }}</h2>
+          <h3>{{ artist }}</h3>
+        </div>
+      </vs-card>
+    </vs-col>
+  </vs-row>
 </template>
 
 <script>
@@ -22,15 +22,26 @@
     name: "MusicPlayer",
     data: function() {
       return { 
-        title: "Hello World!"
+        title: "Fetching data...",
+        artist: "Someone",
+        image: require("@/assets/no_song.png")
       }
     },
     mounted: async function() {
       try {
         let response = await axios.get("http://localhost:3000/spotify");
-        console.log(response.data);
+        
+        let spotify_data = response.data;
+        this.artist = spotify_data.artists[0].name;
+        this.title = spotify_data.album.name;
+        this.image = spotify_data.album.images[1].url;
       } catch (error) {
         console.error(error);
+      }
+    },
+    methods: {
+      prepareImage: function(img) {
+        return require(img);
       }
     }
   }
